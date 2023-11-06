b4e04292c863b9ce261e125f6ee34ea61d18c441
Daniel Hughes <147650482+DanielHughes1580@users.noreply.github.com>
2023-11-06 22:02:08 +0000

    PyPoll.py

--- PyPoll.py
+++ PyPoll.py
@@ -12,7 +12,7 @@ rad = "Raymon Anthony Doane"
 radtot = 0
 
 # Creating Pathway
-csvpath = os.path.join('..', 'Resources','election_data.csv')
+csvpath = os.path.join('election_data.csv')
 
 #  Main Code
 with open(csvpath) as csvfile:
